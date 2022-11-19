3d75879b1706777b688569731b7b4d79873a5cd1
Masuduzzaman Mohammad <masuduzzaman_811@outlook.com>
2022-11-19 12:16:11 +0600

    make the optional suggested change

--- decorator.rb
+++ decorator.rb
@@ -19,10 +19,6 @@ end
 
 class TrimmerDecorator < Decorator
   def correct_name
-    if @nameable.correct_name.size >= 11
-      @nameable.correct_name.slice(0, 10)
-    else
-      @nameable.correct_name
-    end
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
