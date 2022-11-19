0bab6eba67ce993a19e691257f5eeb0756550638
Masuduzzaman Mohammad <masuduzzaman_811@outlook.com>
2022-11-19 14:01:55 +0600

    fix the issues as the change required from PR review

--- person.rb
+++ person.rb
@@ -26,8 +26,8 @@ class Person < Nameable
     @name
   end
 
-  def add_rental(date, person)
-    Rental.new(date, person, self)
+  def add_rental(date, book)
+    Rental.new(date, book, self)
   end
 
   private :of_age
